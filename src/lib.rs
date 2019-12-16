655beb3d35164962562b628552b63447bda2a650
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-16 16:01:42 -0500

    [Cargo] Create `bay` library crate

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! A content-addressable data store for Ocean.
+//!
+//! This project is a work in progress. If you want to help make this a reality,
+//! contact [Nikolai Vazquez].
+//!
+//! [Nikolai Vazquez]: https://twitter.com/NikolaiVazquez
+
+#![deny(missing_docs)]
