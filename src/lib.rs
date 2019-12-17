0a93d8316f45584554751d62d0b004cdbc4f9b01
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-16 23:54:54 -0500

    Improve crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
-//! The content-addressable data store for Ocean.
+//! The content-addressable data store for [Ocean].
 //!
 //! This project is a work in progress. If you want to help make this a reality,
 //! contact [Nikolai Vazquez].
 //!
+//! # Contributing
+//!
+//! See [`CONTRIBUTING.md`] for ways to improve Bay and the Ocean ecosystem.
+//!
+//! All participation must abide by the [Ocean Code of Conduct].
+//!
+//! # FAQ
+//!
+//! - **Q:** What is Ocean?
+//!
+//!   **A:** [Ocean] is a cross-platform package manager that focuses on ease of
+//!   use, performance, and flexibility.
+//!
+//! [Ocean]:                 https://github.com/oceanpkg/ocean
+//! [Ocean Code of Conduct]: https://github.com/oceanpkg/ocean/blob/master/CODE_OF_CONDUCT.md
+//!
 //! [Nikolai Vazquez]: https://twitter.com/NikolaiVazquez
+//! [`CONTRIBUTING.md`]: https://github.com/oceanpkg/bay/blob/master/CONTRIBUTING.md
 
 #![deny(missing_docs)]
